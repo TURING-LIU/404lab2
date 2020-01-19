f7288ded253a7193ebff3801eb76195f717c04e0
TURING-LIU <56138560+TURING-LIU@users.noreply.github.com>
2020-01-18 20:36:18 -0700

    lab2

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ def create_tcp_socket():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except (socket.error,msg):
-        print('Failed to create socket. Error code:,')
+        print('Failed to create socket. Error code: ')
         sys.exit()
     print("Socket created successfully")
     return s
